44b0281daca0f13168d57a78dd9f4da96986f93f
Cole Martin <mar08049@gmail.com>
2017-11-17 22:45:26 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+      if user.save
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        render :new
+      end
+  end
+
+  def show
+      @user = User.find(params[:id])
+  end
+
+  private
+
+  def require_login
+    redirect_to root_path unless session.include? :user_id
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+  end
+end
